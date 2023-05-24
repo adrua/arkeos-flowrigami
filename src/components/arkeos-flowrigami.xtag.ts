eb5084ef82950261bed6fd62ad6949c5a24b3876
Adalberto Rua <adalbertor@solin.com.co>
2023-05-24 04:39:49 -0500

    ajustes linea base

--- src/components/arkeos-flowrigami.xtag.ts
+++ src/components/arkeos-flowrigami.xtag.ts
@@ -1,6 +1,7 @@
 import Flowrigami = require("../../../dist/flowrigami/index");
 
 declare var XTagElement: any;
+declare var xtag: any;
 
 export class ArkeosFlowrigami extends XTagElement  {
     private flowrigami: any;
@@ -9,20 +10,25 @@ export class ArkeosFlowrigami extends XTagElement  {
 
     private promise: Promise<void> = new Promise<void>((resolve) => resolve());
 
-    private _artifacto = "";
-    set 'artifacto::attr'(val: any) {
-        this._artifacto = val;
+    set 'source::attr'(val: string) {
+        this.promise?.then(() => {
+            if (!this.flowrigami && val) {
+                this.flowrigami = new Flowrigami.Flowrigami(this.host.firstElementChild, { viewMode: this._view_mode });
+            }
+    
+            val && this.flowrigami?.diagramApi?.import(val);
+        });
     }
 
-    get 'artifacto::attr'(): any {
-        return this._artifacto
+    get 'source::attr'(): string {
+        return this.flowrigami?.diagramApi?.export();
     }
 
     private _view_mode: boolean = null;
-    set 'view-mode::attr'(val: boolean) {
+    set 'viewMode::attr'(val: boolean) {
         this._view_mode = val;
 
-        this.promise.then(() => {
+        this.promise?.then(() => {
             if (this.flowrigami) {
                 this.flowrigami.unmount();
             }
@@ -31,7 +37,7 @@ export class ArkeosFlowrigami extends XTagElement  {
         });
     }
 
-    get 'view-mode::attr'(): boolean {
+    get 'viewMode::attr'(): boolean {
         return this._view_mode
     }
 
@@ -44,34 +50,6 @@ export class ArkeosFlowrigami extends XTagElement  {
         this.flowrigami.diagramApi.import(await response.text());
     }
 
-    'click::event:delegate(#arkeos-flowrigami-export)'(e: PointerEvent) {
-        const _this = (e as unknown as any)["path"][1];
-        const json = _this.flowrigami.diagramApi.export();
-        const blob = new Blob([json], { type: 'application/json' });
-        const dataUrl = URL.createObjectURL(blob);
-
-        _this.anchor.download = 'diagram.json';
-        _this.anchor.href = dataUrl;
-
-        _this.promise.then(() => URL.revokeObjectURL(_this.anchor.href));
-    }
-
-    'click::event:delegate(#indicator-form)'(e: PointerEvent) {
-        const _this = (e as unknown as any)["path"].find((item: HTMLElement) => item.localName == "arkeos-florigami");
-        e.preventDefault();
-
-        const indicatorId = _this.host.getElementById('indicatorId') as HTMLInputElement;
-        const indicatorValue = _this.host.getElementById('indicatorValue') as HTMLInputElement;
-        const indicatorColor = _this.host.getElementById('indicatorColor') as HTMLInputElement;
-    
-        const id = indicatorId.value;
-        const parsedValue = Number(indicatorValue.value);
-        const value = parsedValue === parsedValue ? parsedValue : indicatorValue.value;
-        const color = indicatorColor.value || undefined;
-
-        _this.flowrigami.indicatorApi.setIndicatorValue(id, value, color);  
-    }
-
     exportData() {
         this.anchor.click();
     }
@@ -80,7 +58,11 @@ export class ArkeosFlowrigami extends XTagElement  {
         super();
 
         this.host = this as unknown as HTMLElement;
-        this.host.setAttribute("style", "width: 100%; height:100%; overflow: none;");
+        this.host.style.width = "100%"; 
+        this.host.style.height = "100%"; 
+        this.host.style.overflow = "none";
+
+        let _this = this;
 
         this.render().then(() => {
             this.anchor = this.host.querySelector("#arkeos-flowrigami-export");    
@@ -88,11 +70,24 @@ export class ArkeosFlowrigami extends XTagElement  {
             if(this._view_mode === null) {
                 this["view-mode"] = true; 
             }    
+        }).then(() => {
+
+            this.flowrigami.main.workspace.store.subscribe('SET_NODE', (e: string) => {
+                let store = _this.flowrigami.main.workspace.store;
+                switch(true) {
+                    case !!store.selectedNode:
+                        xtag.fireEvent(_this, 'selectedNode',{ detail: store.selectedNode });
+                        break;
+                    case !!store.selectedConnector:
+                        xtag.fireEvent(_this, 'selectedConnector', { detail: store.selectedNode });
+                        break;
+                }
+            });
         });
     }
 
     '::template'() {        
-        return `<div style="width: 100%; height:100%; overflow: none;">i'm here</div><a id="arkeos-flowrigami-export" style="width: 0px; height: 0px;"></a>`;
+        return `<div id="arkeos-flowrigami-root" style="width: 100%; height:100%; overflow: none;">i'm here</div><a id="arkeos-flowrigami-export" style="width: 0px; height: 0px;"></a>`;
     }
 }
 
